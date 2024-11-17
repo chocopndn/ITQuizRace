7ff7726632348b482027284b447040cbe8ec7b19
chocopndn <chocopndn@gmail.com>
2024-11-18 04:16:10 +0800

    ADD: AI names

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Leaderboard.css";
+import aiNames from "../../assets/json/aiNames.json";
 
 const Leaderboard = ({ playerName, playerScore, aiScore }) => {
+  const [aiName, setAiName] = useState("AI");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * aiNames.length);
+    setAiName(aiNames[randomIndex]);
+  }, []);
+
   return (
     <div className="leaderboard">
       <h3>Leaderboard</h3>
@@ -11,7 +19,7 @@ const Leaderboard = ({ playerName, playerScore, aiScore }) => {
           <span className="value">{playerScore}</span>
         </div>
         <div className="score">
-          <span className="label">AI:</span>
+          <span className="label">{aiName}:</span>
           <span className="value">{aiScore}</span>
         </div>
       </div>
